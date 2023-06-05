149bf154dd414b64c3e0b8dcab737078c379a795
Snehalaxmi-S <snehalaxmi2104@gmail.com>
2023-06-05 22:13:51 +0530

    Created home and dishes page

--- script.js
+++ script.js
@@ -12,9 +12,23 @@ window.onscroll = () =>{
 }
 
 document.querySelector('#search-icon').onclick = () =>{
-    document.querySelector('search-form').classList.toggle('active');
+    document.querySelector('#search-form').classList.toggle('active');
 }
 
 document.querySelector('#close').onclick = () =>{
-    document.querySelector('search-form').classList.remove('active');
+    document.querySelector('#search-form').classList.remove('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop:true,
+});
