329d255b109edfacbde1a9d72ff22b4fbe5a5d54
Snehalaxmi-S <snehalaxmi2104@gmail.com>
2023-06-07 15:38:56 +0530

    Review section added

--- script.js
+++ script.js
@@ -18,17 +18,41 @@ document.querySelector('#search-icon').onclick = () =>{
 document.querySelector('#close').onclick = () =>{
     document.querySelector('#search-form').classList.remove('active');
 }
-
 var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
     centeredSlides: true,
     autoplay: {
         delay: 7500,
         disableOnInteraction: false,
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
     loop:true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
 });
